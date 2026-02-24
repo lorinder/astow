d68e7fb7a465c1ad70bfaa59e60c75a6b88f0014
Lorenz Minder <lminder@gmx.net>
2026-02-24 12:07:26 -0600

    KissDList:  Add.

---
+++ src-exe/KissDList.hs
@@ -0,0 +1,26 @@
+module KissDList (
+    KissDList
+  , singleton
+  , fromList
+  , toList
+) where
+
+newtype KissDList a = KissDList ([a] -> [a])
+
+instance Semigroup (KissDList a) where
+    (KissDList f) <> (KissDList g) = KissDList (f . g)
+
+instance Monoid (KissDList a) where
+    mempty = empty
+
+empty :: KissDList a
+empty = KissDList id
+
+singleton :: a -> KissDList a
+singleton x = fromList [x]
+
+fromList :: [a] -> KissDList a
+fromList xs = KissDList (\l -> xs ++ l)
+
+toList :: KissDList a -> [a]
+toList (KissDList f) = f []
